e472adb63d466a43265e4b8a8c8b498469ece602
Marwan Kallal <marwan.kallal@gmail.com>
2016-02-07 15:35:49 -0500

    added defs

--- splitter.py
+++ splitter.py
@@ -1,17 +1,26 @@
 
 
-words = set(["ab", "bc", "bb"])
-string = "abbc"
-strlen = len(string)
+def split():
+    words = set(["ab", "bc", "bb"])
+    string = "abbc"
+    strlen = len(string)
 
-found = dict()
+    found = dict()
 
-for start in range(0, strlen):
-    for end in range(strlen, start, -1):
-        test_str = string[start:end]
-        if test_str in words:
-            found[(start, end - 1)] = test_str
+    for start in range(0, strlen):
+        for end in range(strlen, start, -1):
+            test_str = string[start:end]
+            if test_str in words:
+                found[(start, end - 1)] = test_str
 
-for key in found.keys():
-    #TODO: assemble into sentences
+    for key in found.keys():
+        #TODO: assemble into sentences
+        print("TODO assemble")
 
+def word_dfs(word, found):
+    print("TODO search")
+
+def adj_words(word, found):
+    print("TODO adjascent")
+
+split()
