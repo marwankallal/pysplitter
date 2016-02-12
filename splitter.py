0b927b434f83691cd86388a9d62ae4b86d49acf5
Marwan Kallal <marwan.kallal@gmail.com>
2016-02-12 09:38:13 -0500

    number of displayed sentences as function parameter

--- splitter.py
+++ splitter.py
@@ -1,6 +1,7 @@
 ssid = "hellogoodbye"
 dictionary = set(["hell", "hello", "good", "bye", "goodbye"])
 
+# Generator that produces every possible sentence
 def combos(str):
     for i in range(1, len(str)):
         start = str[0:i]
@@ -12,8 +13,8 @@ def combos(str):
             yield result
 
 
-
-def split(ssid, words):
+# Get each sentence, rank it, and display top n
+def split(ssid, words, top):
 	sentences = list()
 
 	for combo in combos(ssid):
@@ -21,9 +22,12 @@ def split(ssid, words):
 		sentences.append((score(combo, words), combo))
 	sentences.sort(reverse=True)
 
-	for i in range(0, 10):
+	for i in range(0, top):
 		print(sentences[i])
 
+
+
+# Ranking function to get the best sentences
 def score(sentence, words):
 	# set to desired function
 	wordcnt = 0
@@ -33,4 +37,4 @@ def score(sentence, words):
 	return wordcnt
 
 
-split(ssid, dictionary)
+split(ssid, dictionary, 10)
