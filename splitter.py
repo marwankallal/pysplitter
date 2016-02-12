022bba0df522d9d1e12b7e925a078d69237c334a
Marwan Kallal <marwan.kallal@gmail.com>
2016-02-11 21:09:14 -0500

    built with generator

--- splitter.py
+++ splitter.py
@@ -1,26 +1,36 @@
-
-
-def split():
-    words = set(["ab", "bc", "bb"])
-    string = "abbc"
-    strlen = len(string)
-
-    found = dict()
-
-    for start in range(0, strlen):
-        for end in range(strlen, start, -1):
-            test_str = string[start:end]
-            if test_str in words:
-                found[(start, end - 1)] = test_str
-
-    for key in found.keys():
-        #TODO: assemble into sentences
-        print("TODO assemble")
-
-def word_dfs(word, found):
-    print("TODO search")
-
-def adj_words(word, found):
-    print("TODO adjascent")
-
-split()
+ssid = "hellogoodbye"
+dictionary = set(["hell", "hello", "good", "bye", "goodbye"])
+
+def combos(str):
+    for i in range(1, len(str)):
+        start = str[0:i]
+        end = str[i:]
+        yield (start, end)
+        for split in combos(end):
+            result = [start]
+            result.extend(split)
+            yield result
+
+
+
+def split(ssid, words):
+	sentences = list()
+
+	for combo in combos(ssid):
+		wordcnt = 0
+		sentences.append((score(combo, words), combo))
+	sentences.sort(reverse=True)
+
+	for i in range(0, 10):
+		print(sentences[i])
+
+def score(sentence, words):
+	# set to desired function
+	wordcnt = 0
+	for word in sentence:
+		if word in words:
+			wordcnt += 1
+	return wordcnt
+
+
+split(ssid, dictionary)
